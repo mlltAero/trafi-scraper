d7116bfd93d4d10ce6234a3316a21607b3d9dbcf
mlltCSS <mindaugas.leonas12345@gmail.com>
2020-11-02 18:23:38 +0200

    commit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cloudscraper
+import json
 
 scraper = cloudscraper.create_scraper()  # returns a CloudScraper instance
 # Or: scraper = cloudscraper.CloudScraper()  # CloudScraper inherits from requests.Session
-print(scraper.get("https://web.trafi.com/api/map-markers/vilnius/transports?bounds=54.7818886799772%2C25.437952041625977%3B54.58977893569762%2C25.437952041625977%3B54.58977893569762%2C25.13445472717285%3B54.7818886799772%2C25.13445472717285").text)  # => "<!DOCTYPE html><html><head>..."
+file = scraper.get("https://web.trafi.com/api/map-markers/vilnius/transports?bounds=54.7818886799772%2C25.437952041625977%3B54.58977893569762%2C25.437952041625977%3B54.58977893569762%2C25.13445472717285%3B54.7818886799772%2C25.13445472717285").text  # => "<!DOCTYPE html><html><head>..."
+with open('received_file', 'w') as outfile:
+    json.dump(file, outfile)
+
