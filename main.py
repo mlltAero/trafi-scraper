7e9da5bc119a719d940362ffa14c18b2ebe50dd8
mlltCSS <mindaugas.leonas12345@gmail.com>
2020-11-09 20:08:59 +0200

    commit

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import requests
 import json
+import time
 
-file = requests.get('https://web.trafi.com/api/additional-transport/vilnius/carsharing')
-car = json.loads(file.text)
-for i in car["cars"]:
-    x = i["car"]["plateNumber"]
-    y = i["car"]["coordinate"]
-    print(x)
-    print(y["lat"])
-    print(y["lng"])
+timer = 1
+while timer == 1:
+    times = 0
+    file = requests.get('https://web.trafi.com/api/additional-transport/vilnius/carsharing')
+    car = json.loads(file.text)
+    for i in car["cars"]:
+        x = i["car"]["plateNumber"]
+        y = i["car"]["coordinate"]
+        print(x)
+        print(y["lat"])
+        print(y["lng"])
+        times =+ 1
+    print("timecount: ", times)
+    print("sec: ", times*60)
+    time.sleep(60)
